0812e295bf0080588de7f28fd16c71f4e8689cf1
Aral Tasher <araltasher@gmail.com>
2019-05-06 15:32:59 -0400

    implented the main pages and react-router

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,21 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
+
+//  Import CSS
+import css from './styles/style.styl';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid} />
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById('root'));
