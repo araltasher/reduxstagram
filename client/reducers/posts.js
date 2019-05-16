9fa200f9b6411296c2357eb4d3b59593b7068f9d
Aral Tasher <araltasher@gmail.com>
2019-05-15 21:18:12 -0400

    likes reducer added

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,5 +3,16 @@
 //  2-  The copy of the current state
 
 export default function posts(state = [], action) {
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      //  return the updated state
+      return [
+        ...state.slice(0, i),
+        { ...state[i], likes: state[i].likes + 1 },
+        ...state.slice(i + 1)
+      ];
+    default:
+      return state;
+  }
 }
